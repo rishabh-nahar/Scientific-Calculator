2a61001e9fe53676aa4ef115cbf478a30af4d345
rishabh-nahar <90189129+rishabh-nahar@users.noreply.github.com>
2021-12-08 23:52:50 +0530

    Add files via upload

---
+++ javadocfile/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"scientific_cal.SciCal","c":"SCalci","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"scientific_cal.SciCal","c":"SCalci","l":"SCalci()","url":"%3Cinit%3E()"}]
